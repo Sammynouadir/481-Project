cd9e9376bfda88295ec4739dcf98acafd588e610
wedward6 <wedward6@emich.edu>
2025-03-18 22:35:19 -0400

    fixed keys

--- FireTruckSim/ValveControllers/runValves.bat
+++ FireTruckSim/ValveControllers/runValves.bat
@@ -2,6 +2,6 @@
 start python valveController.py 8161 "discharge 1 position"
 start python valveController.py 8162 "discharge 2 position"
 start python valveController.py 8163 "discharge 3 position"
-start python valveController.py 8164 "intake Position"
-start python valveController.py 8165 "tank to pump"
+start python valveController.py 8164 "intake position"
+start python valveController.py 8165 "tank to pump position"
 start python valveController.py 8166 "tank fill position"
