144cde367bd285f73290b4e0bfeea3dbb982cef8
JasonRaab <69011410+JasonRaab@users.noreply.github.com>
2025-04-03 18:59:26 -0400

    Adding launcher
    The first time you run this you will need to run "npm install" in the governor and PTO dirs. You will also need .net 8.0 and pythonnet on your system

---
+++ Launcher.bat
@@ -0,0 +1,11 @@
+@echo off
+set "SCRIPT_DIR=%~dp0"
+copy /Y "FireTruckSim\PumpBrainHost\PumpBrain\bin\Debug\net7.0\PumpBrain.dll" "%SCRIPT_DIR%PumpBrain.dll"
+start python controlpanel.py
+start python FireTruckSim\FoamController\foam_controller.py
+start python FireTruckSim\Gauges\gaugesUDP.py
+start cmd /c "npm start --prefix FireTruckSim\Governor"
+start cmd /c "npm start --prefix FireTruckSim\PTO\PTO"
+pushd FireTruckSim
+pushd ValveControllers
+call runValves.bat
